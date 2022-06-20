174b7155cf04efdb6500521ee7c92fae7b3d5d64
Vadim <mrvadim06@gmail.com>
2022-06-20 20:30:56 +0300

    choose word

--- js/hengmen.js
+++ js/hengmen.js
@@ -1,3 +1,8 @@
 
 const words = ["яблоко", "машина", "реструктуризатор", "стол", "стул", "книга", "ручка", "монитор", "мышка", "конец"]
 
+function chooseWordArr() {
+    console.log(words[Math.floor(Math.random()*10)])
+}
+
+chooseWordArr(words)
