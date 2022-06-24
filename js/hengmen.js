3bcbf2df5de23e3d374fad61c22f4c46a9e0cc12
Vadim <mrvadim06@gmail.com>
2022-06-24 19:08:40 +0300

    change znach

--- js/hengmen.js
+++ js/hengmen.js
@@ -1,13 +1,29 @@
 
 const words = ["яблоко", "машина", "реструктуризатор", "стол", "стул", "книга", "ручка", "монитор", "мышка", "конец"]
 
-function chooseWordArr() {
-    console.log(words[Math.floor(Math.random()*10)])
+ function chooseWordArr() {
+    return words[Math.floor(Math.random()*10)]
+    
 }
+const wordRandom = chooseWordArr()
+ console.log(wordRandom)
 
-chooseWordArr(words)
+// =========================================
 
 function changeImg() {
     document.getElementById("imger").src="/img/2.png"
 }
 
+// ===================================
+
+// количество букв в слове
+
+
+function emphasizeLetter (){
+    chooseWordArr(words).map(item )
+        document.getElementById("border").style.border = "1px solid black"
+        document.getElementById("border").style.margin = "0px 5px 20px"
+        document.getElementById("border").style.width = "50px"
+    
+}
+emphasizeLetter ()
